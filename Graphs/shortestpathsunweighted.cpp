85572aaace57223da49ec5143963d58d7023a6d5
Pradnesh Kalkar <52340022+PradKalkar@users.noreply.github.com>
2020-08-25 14:36:26 +0530

    Shortest paths using bfs
    shortest paths in an unweighted graph using bfs

--- Graphs/shortestpathsunweighted.cpp
+++ Graphs/shortestpathsunweighted.cpp
@@ -1,39 +1,52 @@
-//given an unweighted graph find the shortest path from the start vertex to every other vertex in linear time
-//using bfs
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int n, m;
-    //n is the number of vertices and m the number of edges
     cin >> n >> m;
-    vector<int> graph[n];
+    //n is the number of vertices and m is the number of edges
+    vector<vector<int> > adj(n);
     for (int i = 0; i < m; i++)
     {
         int x, y;
         cin >> x >> y;
-        graph[x].push_back(y);
-        graph[y].push_back(x);
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
-    vector<bool> visited(n);
+    vector<int> parent(n), level(n, -1);
     queue<int> bfs;
     int start;
     cin >> start;
     bfs.push(start);
-    vector<int> dist(n, INT32_MAX);
-    dist[start] = 0;
+    parent[start] = -1, level[start] = 0;
     while (!bfs.empty())
     {
         int node = bfs.front();
         bfs.pop();
-        visited[node] = true;
-        for (auto i : graph[node]) {
-            if (visited[i]) continue;
-            dist[i] = min(dist[i], dist[node]+1);
+        for (auto i : adj[node])
+        {
+            if (level[i] != -1) continue; //implies that u have already visited i
+
+            level[i] = level[node]+1;
+            parent[i] = node;
             bfs.push(i);
         }
     }
-    for (int i = 0; i < n; i++) cout << dist[i] << ' ';
+    //enter the destination for which u wanna find the shortest path to
+    int dest;
+    cin >> dest;
+    cout << "The length of the shortest path is " << level[dest] << '\n';
+    //now printing the shortest path using backtracking the parent pointers
+    vector<int> path;
+    int x = dest;
+    while (x != -1)
+    {
+        path.push_back(x);
+        x = parent[x];
+    }
+    reverse(path.begin(), path.end());
+    cout << "One of the shortest paths is ";
+    for (auto i : path) cout << i << ' ';
     cout << '\n';
 }
